6db66e6d6ee3d2cbc3bb6ca4a1dfd08fc00f7d49
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2023-03-12 15:48:32 -0700

    email button works :)

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,6 +5,9 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 function Header() {
+    function handleClick() {
+        window.location.href = 'mailto:joankarstrom@gmail?subject=Portfolio Site Inquiry';
+      }
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between bg-orange-300/75 z-100'>
         <motion.div 
@@ -59,15 +62,17 @@ function Header() {
         }}
         className="flex flex-row items-center cursor-pointer">
             {/* Avatar*/}
-            <SocialIcon
+            <button onClick={handleClick}>
+                <SocialIcon
                 className='cursor-pointer'
                 network='email'
                 fgColor='#dde6d5' 
                 bgColor='transparent'
-            />
-            <p className="uppercase hidden font-bold md:inline-flex text-sm text text-[#f8c9b8]">
-                Contact Me
-            </p>
+                />
+                <p className="uppercase hidden font-bold md:inline-flex text-sm text text-[#f8c9b8]">
+                    Contact Me
+                </p>
+            </button>
         </motion.div>
     </header>
   )
